6a411c6738945abaae9bfa74ef38948c719c9d83
letoska <95180314+letoska@users.noreply.github.com>
2022-01-04 16:55:16 +0300

    Update police.py

--- police.py
+++ police.py
@@ -1,45 +1,27 @@
-with open ('1.txt', encoding="utf-8") as file1:
-    account1 = len(file1.read())
-with open('1.txt', encoding="utf-8") as file1:
-    text1 = file1.read()
-with open ('2.txt', encoding="utf-8") as file2:
-    account2 = len(file2.read())
-with open('2.txt', encoding="utf-8") as file2:
-    text2 = file2.read()
-with open ('3.txt', encoding="utf-8") as file3:
-    account3 = len(file3.readlines())
-with open('3.txt', encoding="utf-8") as file3:
-    text3 = file3.read()
-if account1 > account2 and account2 > account3:
-    with open ('all.txt', 'w', encoding="utf-8") as all:
-        all.write('1.txt\n')
-        all.write(str(account1))
-        all.write(text1)
-        all.write('2.txt\n')
-        all.write(str(account2))
-        all.write(text2)
-        all.write('3.txt\n')
-        all.write(str(account3))
-        all.write(text3)
-if account2 > account1 and account1 > account3:
-    with open ('all.txt', 'w', encoding="utf-8") as all:
-        all.write('2.txt\n')
-        all.write(str(account2))
-        all.write(text2)
-        all.write('1.txt\n')
-        all.write(str(account1))
-        all.write(text1)
-        all.write('3.txt\n')
-        all.write(str(account3))
-        all.write(text3)
-if account3 > account2 and account2 > account1:
-    with open('all.txt', 'w', encoding="utf-8") as all:
-        all.write('3.txt\n')
-        all.write(str(account3))
-        all.write(text3)
-        all.write('2.txt\n')
-        all.write(str(account2))
-        all.write(text2)
-        all.write('1.txt\n')
-        all.write(str(account1))
-        all.write(text1)
+from pathlib import Path
+import sys, os
+
+source_dir = Path('C:/Users/Zharinovi/Desktop/Python/Pycharm/open files/Ex3/')
+files = source_dir.glob('*.txt')
+file_list = []
+
+
+def get_files(files):
+    for file in files:
+        with file.open('r', encoding='UTF-8') as file_handle:
+            lenght_file = sum(1 for line in open(file, 'r', encoding='UTF-8'))
+            filename = os.path.basename(file)
+            file_list.append({'filename': filename, 'lenght': lenght_file, 'text': file_handle.read()})
+
+
+def get_result():
+    get_files(files)
+    with open('result', 'w', encoding='UTF-8') as res:
+        file_list.sort(key=lambda file_list: file_list['lenght'])
+        for dict in file_list:
+            res.write(str(dict.get('filename') + '\n'))
+            res.write(str(dict.get('lenght')) + '\n')
+            res.write(str(dict.get('text') + '\n'))
+
+
+get_result()
